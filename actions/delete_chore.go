e6aeb112c349e890fe63242a5cb0dffdd25f87a5
David Saah <dasorange.hope@gmail.com>
2024-03-07 17:45:33 +0000

    feat: add DeleteChore function

---
+++ actions/delete_chore.go
@@ -0,0 +1,25 @@
+package actions
+
+import (
+	"log"
+
+	"github.com/DaveSaah/choreflow-api/db"
+)
+
+func DeleteChore(cid uint) error {
+	conn, err := db.Init()
+	if err != nil {
+		log.Printf("Cannot create the db connection: %s\n", err)
+		return err
+	}
+
+	defer conn.Close()
+
+	_, err = conn.Exec(`DELETE FROM Chores WHERE cid=?`, cid)
+	if err != nil {
+		log.Printf("Cannot delete chore: %s\n", err)
+		return err
+	}
+
+	return nil
+}
