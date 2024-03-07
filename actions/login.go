a07337c899ceae8c7a342c9ae0361dbed55ad7c7
David Saah <dasorange.hope@gmail.com>
2024-03-07 17:45:58 +0000

    feat: add login function

---
+++ actions/login.go
@@ -0,0 +1,61 @@
+package actions
+
+import (
+	"database/sql"
+	"log"
+
+	"github.com/DaveSaah/choreflow-api/db"
+	"github.com/DaveSaah/choreflow-api/types"
+	"golang.org/x/crypto/bcrypt"
+)
+
+// func getHashPassword(password string) (string, error) {
+// 	bytePassword := []byte(password)
+// 	hash, err := bcrypt.GenerateFromPassword(bytePassword, bcrypt.DefaultCost)
+// 	if err != nil {
+// 		return "", err
+// 	}
+//
+// 	return string(hash), nil
+// }
+
+func Login(email, passwd string) (*types.People, error) {
+	conn, err := db.Init()
+	if err != nil {
+		log.Printf("Cannot create the db connection: %s\n", err)
+		return &types.People{}, err
+	}
+
+	defer conn.Close()
+
+	// define variables for the query
+	var person types.People
+
+	err = conn.QueryRow(
+		`SELECT passwd, fname, lname, pid, rid FROM People WHERE email=?`,
+		email,
+	).Scan(
+		&person.Passwd,
+		&person.Fname,
+		&person.Lname,
+		&person.Pid,
+		&person.Rid,
+	)
+
+	switch {
+	case err == sql.ErrNoRows:
+		log.Printf("No user with email: %s", email)
+		return &types.People{}, err
+	case err != nil:
+		log.Printf("Error querying the database: %s", err)
+		return &types.People{}, err
+	}
+
+	err = bcrypt.CompareHashAndPassword([]byte(person.Passwd), []byte(passwd))
+	if err != nil {
+		log.Printf("Password does not match: %s", err)
+		return &types.People{}, err
+	}
+
+	return &person, nil
+}
