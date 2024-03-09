01e60ff6dfcaa92799608256afe8a3cab57da92e
David Saah <dasorange.hope@gmail.com>
2024-03-09 17:53:18 +0000

    feat: define api routes for add-chore, assign-chore, and checking db status

--- main.go
+++ main.go
@@ -1,1 +1,20 @@
 package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("POST /choreflow/api/v1/add-chore", AddChoreHandler)
+	mux.HandleFunc("GET /choreflow/api/v1/status", CheckDBHandler)
+	mux.HandleFunc("POST /choreflow/api/v1/assign-chore/{pid}", AssignChoreHandler)
+
+	log.Println("Starting api server at http://localhost:3211/choreflow/api/v1/")
+
+	if err := http.ListenAndServe(":3211", mux); err != nil {
+		log.Println(err)
+	}
+}
