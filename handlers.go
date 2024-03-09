6adee821a0f5889129b69e7376ba4963318c9bd1
David Saah <dasorange.hope@gmail.com>
2024-03-09 17:52:36 +0000

    feat: add assign-chore handler

--- handlers.go
+++ handlers.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"encoding/json"
 	"log"
 	"net/http"
+	"strconv"
 
 	"github.com/DaveSaah/choreflow-api/actions"
+	"github.com/DaveSaah/choreflow-api/types"
 )
 func CheckDBHandler(w http.ResponseWriter, r *http.Request) {
 	log.Printf("Request from %s to %s %s", r.Host, r.Method, r.URL.Path)
@@ -19,3 +22,34 @@ func CheckDBHandler(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("DB is running"))
 }
 
+func AssignChoreHandler(w http.ResponseWriter, r *http.Request) {
+	log.Printf("Request from %s to %s %s", r.Host, r.Method, r.URL.Path)
+
+	var assignment types.Assignment // initialise assignment type
+	pid_str := r.PathValue("pid")
+
+	// convert to pid from string to int
+	pid, err := strconv.ParseUint(pid_str, 10, 64)
+	if err != nil {
+		http.Error(w, "Chore assignment failed", http.StatusInternalServerError)
+		log.Println(err)
+		return
+	}
+
+	// decode the request body into the assignment type
+	err = json.NewDecoder(r.Body).Decode(&assignment)
+	if err != nil {
+		http.Error(w, "Chore assignment failed", http.StatusInternalServerError)
+		log.Println(err)
+		return
+	}
+
+	err = actions.AssignChore(pid, &assignment)
+	if err != nil {
+		http.Error(w, "Chore assignment failed", http.StatusInternalServerError)
+		log.Println(err)
+		return
+	}
+
+	w.Write([]byte("Chore assigned"))
+}
--- types/types.go
+++ types/types.go
@@ -2,25 +2,25 @@
 package types
 
 type Assignment struct {
-	DateDue       string
-	LastUpdated   string
-	DateCompleted string
-	DateAssign    string
-	Assignmentid  uint
-	Cid           uint
-	Sid           uint
-	WhoAssigned   uint
+	DateDue       string `json:"date-due"`
+	LastUpdated   string `json:"last-updated"`
+	DateCompleted string `json:"date-completed"`
+	DateAssign    string `json:"date-assign"`
+	Assignmentid  uint   `json:"assignmentid"`
+	Cid           uint   `json:"cid"`
+	Sid           uint   `json:"sid"`
+	WhoAssigned   uint   `json:"who-assigned"`
 }
 
 type People struct {
-	Passwd string
-	Fname  string
-	Lname  string
-	Email  string
-	Dob    string
-	Tel    string
-	Pid    uint
-	Rid    uint
-	Fid    uint
-	Gender uint
+	Passwd string `json:"passwd"`
+	Fname  string `json:"fname"`
+	Lname  string `json:"lname"`
+	Email  string `json:"email"`
+	Dob    string `json:"dob"`
+	Tel    string `json:"tel"`
+	Pid    uint   `json:"pid"`
+	Rid    uint   `json:"rid"`
+	Fid    uint   `json:"fid"`
+	Gender uint   `json:"gender"`
 }
