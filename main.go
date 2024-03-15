797ee42458cb9ce3ec6308d70a428c624ad32091
David Saah <dasorange.hope@gmail.com>
2024-03-15 07:18:45 +0000

    feat: define baseURL and getURI function

--- main.go
+++ main.go
@@ -7,16 +7,29 @@ import (
 	httpSwagger "github.com/swaggo/http-swagger/v2"
 )
 
+// define base url and port
+var (
+	baseURL string = "/choreflow/api/v1/"
+	port    string = ":3211"
+)
+
+// getURI returns a string that represents the HTTP method and path
+func getURI(method, endpoint string) string {
+	return method + " " + baseURL + endpoint
+}
+
 func main() {
 	mux := http.NewServeMux()
 
-	mux.HandleFunc("POST /choreflow/api/v1/add-chore", AddChoreHandler)
-	mux.HandleFunc("GET /choreflow/api/v1/status", CheckDBHandler)
-	mux.HandleFunc("POST /choreflow/api/v1/assign-chore/{pid}", AssignChoreHandler)
+	// serve API endpoints
+	mux.HandleFunc(getURI("POST", "add-chore"), AddChoreHandler)
+	mux.HandleFunc(getURI("GET", "status"), CheckDBHandler)
+	mux.HandleFunc(getURI("POST", "assign-chore/{pid}"), AssignChoreHandler)
 
-	log.Println("Starting api server at http://localhost:3211/choreflow/api/v1/")
+	log.Printf("Starting api server at http://localhost%s%s\n", port, baseURL)
+	log.Printf("Starting api docs at http://localhost%s%s%s\n", port, baseURL, "swagger/ui")
 
-	if err := http.ListenAndServe(":3211", mux); err != nil {
+	if err := http.ListenAndServe(port, mux); err != nil {
 		log.Println(err)
 	}
 }
