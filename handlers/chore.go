a245acc9b9d0fc629d8da1419e7822eeab42aa9d
David Saah <dasorange.hope@gmail.com>
2024-03-27 14:47:41 +0000

    feat: add edit chore handler

--- handlers/chore.go
+++ handlers/chore.go
@@ -69,6 +69,57 @@ func deleteChore(w http.ResponseWriter, r *http.Request) {
 	successResponse.write(w)
 }
 
+func editChore(w http.ResponseWriter, r *http.Request) {
+	errorResponse := response{msg: "Chore not edited", code: http.StatusInternalServerError}
+	successResponse := response{msg: "Chore edited", code: http.StatusOK}
+	errorEmptyID := response{msg: "Chore ID is empty", code: http.StatusBadRequest}
+	errorBadID := response{msg: "Chore ID is invalid", code: http.StatusBadRequest}
+	errorEmptyBody := response{msg: "Empty body", code: http.StatusBadRequest}
+
+	id := r.PathValue("id")
+	if id == "" {
+		errorEmptyID.write(w)
+		log.Println(errorEmptyID.msg)
+		return
+	}
+
+	cid, err := strconv.Atoi(id)
+	if err != nil {
+		errorResponse.write(w)
+		log.Println(err)
+		return
+	}
+
+	chorename, err := io.ReadAll(r.Body)
+	if err != nil {
+		errorResponse.write(w)
+		log.Println(err)
+		return
+	}
+
+	chore := string(chorename)
+
+	// check if an empty body was passed
+	if chore == "" {
+		errorEmptyBody.write(w)
+		log.Println(errorEmptyBody.msg)
+		return
+	}
+
+	err = actions.EditChore(cid, chore)
+	if err != nil && err.Error() == "id not found" {
+		errorBadID.write(w)
+		return
+	}
+
+	if err != nil {
+		errorResponse.write(w)
+		log.Println(err)
+		return
+	}
+
+	successResponse.write(w)
+}
 // ChoreHandler handles a request to add, delete, or edit a chore.
 func ChoreHandler(w http.ResponseWriter, r *http.Request) {
 	log.Printf("Request from %s to %s %s", r.Host, r.Method, r.URL.Path)
@@ -78,5 +129,7 @@ func ChoreHandler(w http.ResponseWriter, r *http.Request) {
 		addChore(w, r)
 	case http.MethodDelete:
 		deleteChore(w, r)
+	case http.MethodPatch:
+		editChore(w, r)
 	}
 }
